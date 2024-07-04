656775adcbacc7301eb449ab386a3ff06038c4b5
Iber Joseph Bonilla <111488477+berbonilla@users.noreply.github.com>
2024-07-04 19:13:29 +0800

    Delete Map Test directory

--- Map Test/script.js
+++
@@ -1,9 +0,0 @@
-import * as maptilersdk from '@maptiler/sdk';
-
-maptilersdk.config.apiKey = '6yBy7YQYWSG4WESNZ6L3';
-const map = new maptilersdk.Map({
-container: 'map', // container's id or the HTML element to render the map
-style: maptilersdk.MapStyle.STREETS,
-center: [16.62662018, 49.2125578], // starting position [lng, lat]
-zoom: 1, // starting zoom
-});
